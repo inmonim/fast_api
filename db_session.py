54f7ba973bb464cc3354346c0ec52473eacd38b7
inmonim <cat1083014@gmail.com>
2023-08-04 07:38:24 +0000

    requirements 분리, db 커넥터 수정

--- db_session.py
+++ db_session.py
@@ -13,6 +13,18 @@ db_username = config['db']['username']
 db_password = config['db']['password']
 db_database = config['db']['database']
 
-DB_URL = f'mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}'
+DB_URL = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# def test_connect():
+    
+#     from model import User
+    
+#     with SessionLocal() as db:
+#         test_user = db.query(User).filter(User.id == 1).first()
+        
+#     print(test_user.name)
+
+# test_connect()
